538eb2322e7181fcf2a34f1c82175682ad3b8ef2
Mark <pagetribemail@gmail.com>
2011-10-20 21:30:03 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clipper2::Application.config.secret_token = 'ed8aaa01db04369cb3099d59b538cbfc4fb8ed4404e61bba9671da404393e5d01567bb56c71978a8a82e5ec04d31bc086640b56ce347f2ed29e5eb80ae62cdd8'
